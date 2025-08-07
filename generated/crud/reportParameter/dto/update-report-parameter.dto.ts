4dc04420395e0acc1d6019c16a6eb1530b4bf12b
surya-29 <suryaprakash@divami.com>
2025-08-08 03:46:28 +0530

    clean up the repo

--- generated/crud/reportParameter/dto/update-report-parameter.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateReportParameterDto } from './create-report-parameter.dto';
-
-export class UpdateReportParameterDto extends PartialType(CreateReportParameterDto) {}
--- generated/crud/reportResultMapping/dto/update-report-result-mapping.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateReportResultMappingDto } from './create-report-result-mapping.dto';
-
-export class UpdateReportResultMappingDto extends PartialType(CreateReportResultMappingDto) {}
